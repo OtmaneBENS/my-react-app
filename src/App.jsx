5af3dedfa15c5c99f27064aaab32ab5a38bf284e
OtmaneBENS <134215797+OtmaneBENS@users.noreply.github.com>
2024-08-23 16:23:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import MyNavbar from './components/Navbar';
+import ImageCarousel from './components/ImageCarousel';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+      <ImageCarousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,45 @@
+// src/components/ImageCarousel.js
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const ImageCarousel = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First Slide</h3>
+          <p>Description for the first slide.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second Slide</h3>
+          <p>Description for the second slide.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third Slide</h3>
+          <p>Description for the third slide.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ImageCarousel;
